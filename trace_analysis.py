5af772b80ea8212c2d93c4e683e85af12befff1e
ichatz <ichatz@gmail.com>
2019-01-29 16:46:23 +0100

    Refactoring the function for parsing the trace files from iot-lab and generating the statistics

--- trace_analysis.py
+++ trace_analysis.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
 
-def process_iotlab(tracefile):
+def process_iotlab_aggregated(path, tracefile):
     # Read the ip of each node
-    ips = pd.read_csv(tracefile,
+    ips = pd.read_csv(path + 'addr-' + tracefile + '.cap',
                       sep=';|addr:|/',
                       na_filter=True,
                       usecols=[1, 3, 4],
                       header=None,
-                      nrows=550,
                       names=['prefix', 'node_id', 'addr', 'ip', 'scope'],
                       engine='python').dropna()
 
@@ -16,33 +15,33 @@ def process_iotlab(tracefile):
     ips = ips[ips.scope == '64  scope: global'].reset_index(drop=True).drop(['scope'], axis=1)
 
     # Read the rank of each node
-    rank = pd.read_csv(tracefile,
-                       sep=';|\t|R: | \| OP: ',
+    rank = pd.read_csv(path + 'dodag-' + tracefile + '.cap',
+                       sep=';|R: | \| OP:',
                        na_filter=True,
-                       usecols=[1, 4],
                        header=None,
-                       skiprows=550,
+                       usecols=[1, 3],
                        names=['node_id', 'rank'],
                        engine='python').dropna()
 
     # compute the hop of each node
-    rank = rank[rank['rank'].apply(lambda x: x.isdigit())].reset_index(drop=True)
+    rank['rank'] = rank['rank'].convert_objects(convert_numeric=True)
 
     # Merge all data
     node_ip_and_rank = pd.merge(ips, rank, how='inner').drop_duplicates()
 
     # Load the ICMP traces and parse the RTT
-    packets = pd.read_csv('./traces/out-2019-01JAN-28-01.cap',
-                        sep=';|seq=| hop|time = |ms',
-                        na_filter=True,
-                        usecols=[1, 3, 5],
-                        header=None,
-                        skiprows=799,
-                        names=['node_id', 'seq', 'rtt'],
-                        engine='python').dropna().drop_duplicates()
+    packets = pd.read_csv(path + 'trace-' + tracefile + '.cap',
+                          sep=';|seq=| hop|time = |ms',
+                          na_filter=True,
+                          header=None,
+                          usecols=[1, 3, 5],
+                          names=['node_id', 'seq', 'rtt'],
+                          engine='python').dropna().drop_duplicates()
 
     packets = packets.sort_values(by=['node_id', 'seq'], ascending=True, na_position='first')
     packets = packets[packets['rtt'] > 1]
+
+    max_seq = packets['seq'].max()
 
     # Compute the 2 dimensional array
     d_nodes = {}  # <node_id, DataFrame containing seq and rtt columns>
@@ -55,7 +54,7 @@ def process_iotlab(tracefile):
                 {'seq': [int(packets['seq'][n])], packets['node_id'][n]: [packets['rtt'][n]]})
 
     # create the 2 dimensional array
-    nodes = pd.DataFrame([seq for seq in range(1, 1001)], columns=['seq']).set_index('seq')
+    nodes = pd.DataFrame([seq for seq in range(1, max_seq + 1)], columns=['seq']).set_index('seq')
     for node in d_nodes.keys():
         nodes = nodes.join(d_nodes[node].set_index('seq'))
 
@@ -65,20 +64,68 @@ def process_iotlab(tracefile):
     # The network was configured in order to have three nodes communicating directly with the root.
     rank_to_hops = sorted([int(rank) for rank in list(node_ip_and_rank['rank'].drop_duplicates())])
 
+    # remove root (if it exists)
+    if 256 in rank_to_hops:
+        rank_to_hops.remove(256)
+
     hops = {}
-    icmp = [x for x in range(1,len(nodes)+1)]
     for node in node_ip_and_rank.index:
-        if (rank_to_hops.index(int(node_ip_and_rank['rank'][node]))+1) in hops:
+        if not node_ip_and_rank['node_id'][node] in d_nodes.keys():
+            continue
+
+        if not node_ip_and_rank['rank'][node] in rank_to_hops:
+            continue
+
+        if (rank_to_hops.index(node_ip_and_rank['rank'][node]) + 1) in hops:
             # The key should be created
-            hops[rank_to_hops.index(int(node_ip_and_rank['rank'][node]))+1].append(node_ip_and_rank['node_id'][node])
+            hops[rank_to_hops.index(node_ip_and_rank['rank'][node]) + 1].append(node_ip_and_rank['node_id'][node])
         else:
             # Just append to the list of nodes
-            hops[rank_to_hops.index(int(node_ip_and_rank['rank'][node]))+1] = [node_ip_and_rank['node_id'][node]]
+            hops[rank_to_hops.index(node_ip_and_rank['rank'][node]) + 1] = [node_ip_and_rank['node_id'][node]]
 
+    # construct hop based statistics
+    hop_nodes = pd.DataFrame({1: nodes[hops[1]].mean(axis=1)})
 
-    # Contain mean time for each distance from the root
-    hop_nodes = pd.DataFrame({1: nodes[hops[1]].mean(axis=1),\
-                              2: nodes[hops[2]].mean(axis=1),\
-                              3: nodes[hops[3]].mean(axis=1)})
+    for hop in range(2, max(list(hops.keys())) + 1):
+        hop_nodes[hop] = nodes[hops[hop]].mean(axis=1)
 
     return hop_nodes
+
+
+def separate_outliers(hop_nodes):
+    window_size = 10
+    std_values = pd.DataFrame(columns=[hop for hop in range(1, max(
+        list(hop_nodes.keys())) + 1)])  # Maintain x(t) if mean-2*std <= x(t) <? mean+2*std
+    outliers = pd.DataFrame(
+        columns=[hop for hop in range(1, max(list(hop_nodes.keys())) + 1)])  # Maintain x(t) otherwise
+
+    for h in hop_nodes.keys():
+        # Returns two DataFrames containing standard values and outliers
+        mn = hop_nodes[h].mean()
+        std = hop_nodes[h].std()
+        std_window = pd.Series([])  # Standard values
+        out_window = pd.Series([])  # Outliers
+
+        for window in (hop_nodes[h].groupby(hop_nodes[h].index // window_size * window_size)):
+            std_curr = []
+            out_curr = []
+            for x in window[1]:
+                if mn - 2 * std <= x and x <= mn + 2 * std:
+                    std_curr.append(x)
+                    out_curr.append(None)
+                else:
+                    std_curr.append(None)
+                    out_curr.append(x)
+
+            std_window = std_window.append(pd.Series(std_curr))
+            out_window = out_window.append(pd.Series(out_curr))
+
+        std_values[h] = std_window
+        outliers[h] = out_window
+
+    std_values = std_values.reset_index().drop(columns=['index'])
+    std_values.fillna(value=pd.np.nan, inplace=True)
+    outliers = outliers.reset_index().drop(columns=['index'])
+    outliers.fillna(value=pd.np.nan, inplace=True)
+
+    return std_values, outliers
