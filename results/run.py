7efcb759c58b7c5af1a490f1bd9c03bd7f3c773c
fedebyes <fedeb703@gmail.com>
2019-03-02 19:53:39 +0100

    Results for kmeans

--- results/run.py
+++ results/run.py
@@ -39,12 +39,12 @@ df=pd.read_csv(directory+"/traces/traces.csv", sep='\t', encoding='utf-8')
 results_total=pd.DataFrame()
 
 
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,25)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,50)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,100)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
+#results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,25)
+#results_total=results_total.append(results_kmeans_network,ignore_index = True)
+#results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,50)
+#results_total=results_total.append(results_kmeans_network,ignore_index = True)
+#results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,100)
+#results_total=results_total.append(results_kmeans_network,ignore_index = True)
 results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,200)
 results_total=results_total.append(results_kmeans_network,ignore_index = True)
 
@@ -56,29 +56,29 @@ results_total.to_csv(directory+"results_total.csv", sep='\t', encoding='utf-8')
 #results_kmeans.to_csv(directory+"results_network_kmeans.csv", sep=',', encoding='utf-8')
 
 ##################################
-directory=os.getcwd()+"/cooja3-9nodes/"
-df=pd.read_csv(directory+"/traces/traces.csv", sep='\t', encoding='utf-8')
-#df=get_traces_csv(directory)
-
-
-#print(df)
-
-results_total=pd.DataFrame()
-
-
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,25)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,50)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,100)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
-results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,200)
-results_total=results_total.append(results_kmeans_network,ignore_index = True)
-
-
-results_total.sort_values('case')
-print(results_total)
-
-results_total.to_csv(directory+"results_total.csv", sep='\t', encoding='utf-8')
+# directory=os.getcwd()+"/cooja3-9nodes/"
+# df=pd.read_csv(directory+"/traces/traces.csv", sep='\t', encoding='utf-8')
+# #df=get_traces_csv(directory)
+#
+#
+# #print(df)
+#
+# results_total=pd.DataFrame()
+#
+#
+# results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,25)
+# results_total=results_total.append(results_kmeans_network,ignore_index = True)
+# results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,50)
+# results_total=results_total.append(results_kmeans_network,ignore_index = True)
+# results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,100)
+# results_total=results_total.append(results_kmeans_network,ignore_index = True)
+# results_kmeans_network,results_kmeans_node=analyze_network(directory,df,200,200)
+# results_total=results_total.append(results_kmeans_network,ignore_index = True)
+#
+#
+# results_total.sort_values('case')
+# print(results_total)
+#
+# results_total.to_csv(directory+"results_total.csv", sep='\t', encoding='utf-8')
 
 
